42d73919276e19795d8565b353490953c15d5e43
Bhavik <bhavikrathod.ir@gmail.com>
2026-09-21 12:04:34 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,6 @@
    Do NOT put passwords here - each person types his own password in the app. */
 window.APP_CONFIG = {
   businessName: 'Jyoti Enterprise',
-  idCardUrl:  'https://script.google.com/macros/s/AKfycbzFu2jfonxEhyAOKicaBNEeGTFAH-4eW3QauGX3zWcAhYbx8Tt1hekBuCCMSKyVwNLh/exec?admin=Bhavik2026Amr',
-  gdsCardUrl: 'https://script.google.com/macros/s/AKfycbz16hFmEQ82tTsP0ZHAQlZk0gvHySBFnkRebX-pHWh7NKhg8_1alMhkf9ukLPvUJ-pG/exec?admin=Bhavik-1996'
+  idCardUrl:  'https://script.google.com/macros/s/AKfycbzFu2jfonxEhyAOKicaBNEeGTFAH-4eW3QauGX3zWcAhYbx8Tt1hekBuCCMSKyVwNLh/exec',
+  gdsCardUrl: 'https://script.google.com/macros/s/AKfycbz16hFmEQ82tTsP0ZHAQlZk0gvHySBFnkRebX-pHWh7NKhg8_1alMhkf9ukLPvUJ-pG/exec'
 };
